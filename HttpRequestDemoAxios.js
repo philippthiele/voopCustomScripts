ea129854eec36453775a43bb67a6b6287323bf8c
Philipp Thiele <philipp.thiele@sap.com>
2023-03-14 15:44:37 +0100

    Update HttpRequestDemoAxios.js

--- HttpRequestDemoAxios.js
+++ HttpRequestDemoAxios.js
@@ -12,7 +12,7 @@
 const axios = require('axios');
 
 function main(input) {
-	return axios.get('https://google.com')
+	return axios.get('https://8.8.8.8/resolve?name=google.com')
 	.then(function (response) {
 	  // handle success
 	  input.insert(response.data);
